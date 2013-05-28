a7b290ead990ed108966883b8ed0125cdcbb69f7
Serge Shpikin <serge@shpikin>
2013-05-28 19:14:50 +0400

    Added date picker, fixed setup.

---
+++ src/main/java/eu/oobikwe/gwt/ui/Dummy.java
@@ -0,0 +1,12 @@
+package eu.oobikwe.gwt.ui;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Dummy implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        // intentional no-op
+    }
+
+}
