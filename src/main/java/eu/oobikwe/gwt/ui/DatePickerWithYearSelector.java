9c833f46b63481629bab859c0b744b30c492784f
eurekafag <eurekafag@eureka7.ru>
2013-05-29 23:42:32 +0400

    Added date picker, fixed setup.

---
+++ src/main/java/eu/oobikwe/gwt/ui/DatePickerWithYearSelector.java
@@ -0,0 +1,21 @@
+package eu.oobikwe.gwt.ui;
+
+import com.google.gwt.user.datepicker.client.CalendarModel;
+import com.google.gwt.user.datepicker.client.DatePicker;
+import com.google.gwt.user.datepicker.client.DefaultCalendarView;
+public class DatePickerWithYearSelector extends DatePicker { 
+
+        public DatePickerWithYearSelector() { 
+                super(new MonthAndYearSelector(),new DefaultCalendarView(),new 
+CalendarModel()) ; 
+                MonthAndYearSelector monthSelector = (MonthAndYearSelector) 
+this.getMonthSelector() ; 
+                monthSelector.setPicker(this) ; 
+                monthSelector.setModel(this.getModel()) ; 
+        } 
+
+        public void refreshComponents() { 
+                super.refreshAll() ; 
+        } 
+
+} 
