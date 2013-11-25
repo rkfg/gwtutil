e361d9afaff60cefee94733d36d43ca1d2c34017
Sergey Shpikin <serge@shpikin>
2013-11-25 16:32:23 +0400

    Handy input with addons.

---
+++ src/main/java/ru/ppsrk/gwt/client/bootstrap/InputAddOnHandy.java
@@ -0,0 +1,26 @@
+package ru.ppsrk.gwt.client.bootstrap;
+
+import com.github.gwtbootstrap.client.ui.InputAddOn;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+
+public class InputAddOnHandy extends InputAddOn {
+
+    public InputAddOnHandy() {
+        super();
+        addStyleName("input-append");
+    }
+
+    public InputAddOnHandy(Widget... widgets) {
+        this();
+        for (Widget widget : widgets) {
+            add(widget);
+        }
+    }
+    public InputAddOnHandy(IsWidget... widgets) {
+        this();
+        for (IsWidget widget : widgets) {
+            add(widget);
+        }
+    }
+}
