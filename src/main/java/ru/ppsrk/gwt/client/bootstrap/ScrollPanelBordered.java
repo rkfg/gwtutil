173705a94d9a4f560051b515d18ced6325d5d722
Sergey Shpikin <serge@shpikin>
2013-12-06 16:11:14 +0400

    ScrollPanel with borders.

---
+++ src/main/java/ru/ppsrk/gwt/client/bootstrap/ScrollPanelBordered.java
@@ -0,0 +1,12 @@
+package ru.ppsrk.gwt.client.bootstrap;
+
+import com.google.gwt.user.client.ui.ScrollPanel;
+
+public class ScrollPanelBordered extends ScrollPanel {
+
+    public ScrollPanelBordered() {
+        super();
+        setStyleName("border-bs");
+    }
+
+}
