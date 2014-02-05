5e447306beb4d30e358d615e7503ee34666d5b3c
Sergey Shpikin <serge@shpikin>
2014-02-05 11:15:30 +0400

    Important margin for the flow panel.

---
+++ src/main/java/ru/ppsrk/gwt/client/FlowPanelFixed.java
@@ -0,0 +1,10 @@
+package ru.ppsrk.gwt.client;
+
+import com.google.gwt.user.client.ui.FlowPanel;
+
+public class FlowPanelFixed extends FlowPanel {
+    public FlowPanelFixed() {
+        super();
+        addStyleName("flow-margin");
+    }
+}
