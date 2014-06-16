a145c0b5ca93d4518ffe1667c033c7460e352ae1
eurekafag <eurekafag@eureka7.ru>
2014-06-16 20:03:14 +0400

    Basic domain template entity.

---
+++ src/main/java/ru/ppsrk/gwt/domain/BasicDomain.java
@@ -0,0 +1,21 @@
+package ru.ppsrk.gwt.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BasicDomain {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
