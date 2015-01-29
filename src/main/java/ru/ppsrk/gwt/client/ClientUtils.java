75abac8b572b497d9bf4b873b7d3bbe1210732a6
Sergey Shpikin <serge@shpikin>
2015-01-29 11:39:57 +0300

    Setup locale fix.

--- src/main/java/ru/ppsrk/gwt/client/ClientUtils.java
+++ src/main/java/ru/ppsrk/gwt/client/ClientUtils.java
@@ -781,8 +781,7 @@ public class ClientUtils {
         });
     }
     
-    public static <T extends LocalizableResource> T setupLocale(Class<T> clazz){
-        T messages = GWT.create(clazz);
+    public static <T extends LocalizableResource> T setupLocale(T messages, Class<T> clazz){
         LocaleFactory.put(clazz, messages);
         Cookies.setCookie("locale", LocaleInfo.getCurrentLocale().getLocaleName());
         return messages;
