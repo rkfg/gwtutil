29ef18b6b713b481f97370f29a40fdbb5dad1396
Sergey Shpikin <serge@shpikin>
2015-04-29 16:20:31 +0300

    Tab pane with full height, heading and UiBinder's content.

---
+++ src/main/java/ru/ppsrk/gwt/bootstrap/client/TabPaneFixed.java
@@ -0,0 +1,13 @@
+package ru.ppsrk.gwt.bootstrap.client;
+
+import com.github.gwtbootstrap.client.ui.TabPane;
+import com.google.gwt.user.client.ui.Widget;
+
+public class TabPaneFixed extends TabPane {
+    
+    protected void initWidget(Widget widget, String heading) {
+        setHeight("100%");
+        setHeading(heading);
+        add(widget);
+    }
+}
