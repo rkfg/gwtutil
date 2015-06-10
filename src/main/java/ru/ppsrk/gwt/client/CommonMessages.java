7ef59327aa0ad4d813e690b5a3f74590ccc6dc8a
Sergey Shpikin <serge@shpikin>
2015-06-10 15:54:59 +0300

    Most common messages added.

---
+++ src/main/java/ru/ppsrk/gwt/client/CommonMessages.java
@@ -0,0 +1,51 @@
+package ru.ppsrk.gwt.client;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface CommonMessages extends Messages {
+
+    @DefaultMessage("Добавить")
+    public String add();
+
+    @DefaultMessage("Создать")
+    public String create();
+
+    @DefaultMessage("Изменить")
+    public String change();
+
+    @DefaultMessage("Удалить")
+    public String delete();
+
+    @DefaultMessage("ОК")
+    public String ok();
+
+    @DefaultMessage("Отмена")
+    public String cancel();
+
+    @DefaultMessage("Выход")
+    public String logout();
+
+    @DefaultMessage("Роль")
+    public String role();
+
+    @DefaultMessage("Пользователь")
+    public String user();
+
+    @DefaultMessage("Менеджер")
+    public String manager();
+
+    @DefaultMessage("Администратор")
+    public String admin();
+
+    @DefaultMessage("Загружается...")
+    public String loading();
+
+    @DefaultMessage("С даты")
+    public String fromDate();
+
+    @DefaultMessage("По дату")
+    public String toDate();
+
+    @DefaultMessage("Итого")
+    public String total();
+}
