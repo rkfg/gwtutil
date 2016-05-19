529e81ade16e0e7bbe60a593cf46da00aaf26f01
Sergey Shpikin <serge@shpikin>
2016-05-19 16:04:59 +0300

    Add TypeaheadWrapper.

---
+++ src/main/java/ru/ppsrk/gwt/bootstrap/client/TypeaheadWrapper.java
@@ -0,0 +1,55 @@
+package ru.ppsrk.gwt.bootstrap.client;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.gwtbootstrap.client.ui.TextBox;
+import com.github.gwtbootstrap.client.ui.Typeahead;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
+
+import ru.ppsrk.gwt.client.DecoratorBase;
+import ru.ppsrk.gwt.client.HasListboxValue;
+
+public class TypeaheadWrapper<T extends HasListboxValue> extends DecoratorBase<Typeahead> {
+
+    public TypeaheadWrapper() {
+    }
+
+    public TypeaheadWrapper(Typeahead typeahead) {
+        decorated = typeahead;
+    }
+
+    private Map<String, T> objects = new HashMap<>();
+
+    public void fill(Collection<T> data) {
+        MultiWordSuggestOracle suggestOracle = (MultiWordSuggestOracle) decorated.getSuggestOracle();
+        suggestOracle.clear();
+        objects.clear();
+        for (T elem : data) {
+            String title = elem.getListboxValue();
+            suggestOracle.add(title);
+            objects.put(title, elem);
+        }
+    }
+
+    public T getValue() {
+        return objects.get(getText());
+    }
+
+    public void setText(String text) {
+        ((TextBox) decorated.getWidget()).setValue(text);
+    }
+
+    public String getText() {
+        return ((TextBox) decorated.getWidget()).getValue();
+    }
+
+    @Override
+    protected void checkType(IsWidget w) {
+        if (!(w instanceof Typeahead)) {
+            throw new IllegalArgumentException("Only Typeahead is allowed as a child widget.");
+        }
+    }
+}
