1995e07b3b544572d4e5f3e466af5857f68d7c58
Sergey Shpikin <serge@shpikin>
2016-12-20 13:29:38 +0300

    Add reload event support.

--- src/main/java/ru/ppsrk/gwt/bootstrap/client/AbstractDataGridTabPane.java
+++ src/main/java/ru/ppsrk/gwt/bootstrap/client/AbstractDataGridTabPane.java
@@ -2,19 +2,23 @@ package ru.ppsrk.gwt.bootstrap.client;
 
 import java.util.Collection;
 
-import ru.ppsrk.gwt.client.ClientUtils.MyAsyncCallback;
-import ru.ppsrk.gwt.client.HasId;
-
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.view.client.SingleSelectionModel;
 
 import fr.mikrosimage.gwt.client.CompleteResizableDataGrid;
+import ru.ppsrk.gwt.client.ClientUtils.MyAsyncCallback;
+import ru.ppsrk.gwt.client.event.ReloadDataEvent;
+import ru.ppsrk.gwt.client.event.ReloadDataEvent.ReloadDataHandler;
+import ru.ppsrk.gwt.client.HasId;
 
 public abstract class AbstractDataGridTabPane<T extends HasId> extends TabPaneFixed {
 
     @UiField(provided = true)
     public CompleteResizableDataGrid<T, SingleSelectionModel<T>> dg_data = new CompleteResizableDataGrid<>(new SingleSelectionModel<T>());
+    private EventBus eventBus;
 
     protected void loadData() {
         dg_data.setLoadingData(null);
@@ -32,6 +36,23 @@ public abstract class AbstractDataGridTabPane<T extends HasId> extends TabPaneFi
     @Override
     protected void onShown() {
         dg_data.redraw();
+    }
+
+    public HandlerRegistration registerReload(EventBus eventBus, final Long groupId) {
+        this.eventBus = eventBus;
+        return eventBus.addHandler(ReloadDataEvent.TYPE, new ReloadDataHandler() {
+
+            @Override
+            public void onReloadData(ReloadDataEvent event) {
+                if (event.groupId == null || event.groupId.equals(groupId)) {
+                    loadData();
+                }
+            }
+        });
+    }
+
+    public void broadcastReload(Long groupId) {
+        eventBus.fireEvent(new ReloadDataEvent(groupId));
     }
 
 }
---
+++ src/main/java/ru/ppsrk/gwt/client/event/ReloadDataEvent.java
@@ -0,0 +1,34 @@
+package ru.ppsrk.gwt.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+
+import ru.ppsrk.gwt.client.event.ReloadDataEvent.ReloadDataHandler;
+
+public class ReloadDataEvent extends GwtEvent<ReloadDataHandler> {
+
+    public interface ReloadDataHandler extends EventHandler {
+        public void onReloadData(ReloadDataEvent event);
+    }
+
+    public static final Type<ReloadDataHandler> TYPE = new Type<ReloadDataHandler>();
+    public Long groupId;
+
+    @Override
+    public com.google.gwt.event.shared.GwtEvent.Type<ReloadDataHandler> getAssociatedType() {
+        return TYPE;
+    }
+
+    public ReloadDataEvent() {
+    }
+    
+    public ReloadDataEvent(Long groupId) {
+        this.groupId = groupId;
+    }
+    
+    @Override
+    protected void dispatch(ReloadDataHandler handler) {
+        handler.onReloadData(this);
+    }
+
+}
