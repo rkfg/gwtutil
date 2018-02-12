74893c1a192893ccf9a4b338a5d7a6dee9907101
Sergey Shpikin <serge@shpikin>
2018-02-12 10:24:09 +0300

    Add separator node for cell tree

--- src/main/java/ru/ppsrk/gwt/client/TreeViewHelper.java
+++ src/main/java/ru/ppsrk/gwt/client/TreeViewHelper.java
@@ -1,10 +1,8 @@
 package ru.ppsrk.gwt.client;
 
-import ru.ppsrk.gwt.client.ClientUtils.LoadCellCallback;
-import ru.ppsrk.gwt.client.TreeViewHelper.HasCellValue;
-
 import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.Cell.Context;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NodeList;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
@@ -13,10 +11,53 @@ import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.SingleSelectionModel;
 import com.google.gwt.view.client.TreeViewModel;
 
+import ru.ppsrk.gwt.client.ClientUtils.LoadCellCallback;
+import ru.ppsrk.gwt.client.TreeViewHelper.HasCellValue;
+
 public abstract class TreeViewHelper<T extends HasCellValue> implements LoadCellCallback<T>, TreeViewModel {
 
     public interface HasCellValue extends SettableParent {
         String getCellValue();
+
+        default void render(Context context, HasCellValue value, SafeHtmlBuilder sb) {
+            sb.appendHtmlConstant("<span id='celltree" + value.getId() + "'>").appendEscaped(value.getCellValue())
+                    .appendHtmlConstant("</span>");
+        }
+    }
+
+    @SuppressWarnings("serial")
+    public static class SeparatorCell implements HasCellValue {
+
+        @Override
+        public void setParent(Hierarchic parent) {
+
+        }
+
+        @Override
+        public Hierarchic getParent() {
+            return null;
+        }
+
+        @Override
+        public Long getId() {
+            return null;
+        }
+
+        @Override
+        public void setId(Long newId) {
+
+        }
+
+        @Override
+        public void render(Context context, HasCellValue value, SafeHtmlBuilder sb) {
+            sb.appendHtmlConstant("<hr style='border-top: 1px solid #999' />");
+        }
+
+        @Override
+        public String getCellValue() {
+            return null;
+        }
+
     }
 
     protected SingleSelectionModel<T> selectionModel = new SingleSelectionModel<>();
@@ -25,8 +66,7 @@ public abstract class TreeViewHelper<T extends HasCellValue> implements LoadCell
 
         @Override
         public void render(Context context, HasCellValue value, SafeHtmlBuilder sb) {
-            sb.appendHtmlConstant("<span id='celltree" + value.getId() + "'>").appendEscaped(value.getCellValue())
-                    .appendHtmlConstant("</span>");
+            value.render(context, value, sb);
         }
     };
 
