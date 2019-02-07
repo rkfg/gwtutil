bb7d37804db0bb1e7cafd75ee78be68483f53b42
Sergey Shpikin <serge@shpikin>
2019-02-07 17:36:13 +0300

    Add typical columns creation using method references

--- src/main/java/fr/mikrosimage/gwt/client/CompleteResizableDataGrid.java
+++ src/main/java/fr/mikrosimage/gwt/client/CompleteResizableDataGrid.java
@@ -2,17 +2,25 @@ package fr.mikrosimage.gwt.client;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Function;
 
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.gwt.view.client.Range;
 import com.google.gwt.view.client.SetSelectionModel;
 
+import ru.ppsrk.gwt.client.CommonMessages;
+import ru.ppsrk.gwt.client.DateColumn;
 import ru.ppsrk.gwt.client.HasId;
+import ru.ppsrk.gwt.client.HasListboxValue;
+import ru.ppsrk.gwt.client.LongColumn;
+import ru.ppsrk.gwt.shared.FieldComparator;
 
 public class CompleteResizableDataGrid<T extends HasId, S extends SetSelectionModel<T>> extends ResizableDataGrid<T> {
     private Set<Long> selectedSet = new HashSet<>();
@@ -21,6 +29,7 @@ public class CompleteResizableDataGrid<T extends HasId, S extends SetSelectionMo
     private ListHandler<T> sortHandler = new ListHandler<>(dataProvider.getList());
     private Range visibleRange = null;
     private int scrollPos = 0;
+    private CommonMessages messages = null;
 
     public CompleteResizableDataGrid(S selectionModel) {
         this.selectionModel = selectionModel;
@@ -96,5 +105,54 @@ public class CompleteResizableDataGrid<T extends HasId, S extends SetSelectionMo
 
     public <C> Column<T, C> addColumn(Column<T, C> col, String headerString, Comparator<T> comparator) {
         return addColumn(col, headerString, comparator, true);
+    }
+
+    public void setMessages(CommonMessages messages) {
+        this.messages = messages;
+    }
+
+    public DateColumn<T> addDateColumn(Function<T, Date> col, String headerString) {
+        return (DateColumn<T>) addColumn(new DateColumn<T>(messages) {
+
+            @Override
+            protected Date getDate(T object) {
+                return col.apply(object);
+            }
+        }, headerString, new FieldComparator<>(col));
+    }
+
+    public LongColumn<T> addLongColumn(Function<T, Long> col, String headerString) {
+        return (LongColumn<T>) addColumn(new LongColumn<T>(messages) {
+
+            @Override
+            protected Long getLongValue(T object) {
+                return col.apply(object);
+            }
+        }, headerString, new FieldComparator<>(col));
+    }
+
+    public TextColumn<T> addTextColumn(Function<T, String> col, String headerString) {
+        return (TextColumn<T>) addColumn(new TextColumn<T>() {
+
+            @Override
+            public String getValue(T object) {
+                return col.apply(object);
+            }
+        }, headerString, new FieldComparator<>(col));
+    }
+
+    public TextColumn<T> addHLBColumn(Function<T, HasListboxValue> col, String headerString) {
+        Function<T, String> function = col.andThen(v -> v == null ? null : v.getListboxValue());
+        return (TextColumn<T>) addColumn(new TextColumn<T>() {
+
+            @Override
+            public String getValue(T object) {
+                HasListboxValue result = col.apply(object);
+                if (result == null) {
+                    return messages.notSet();
+                }
+                return result.getListboxValue();
+            }
+        }, headerString, new FieldComparator<>(function));
     }
 }
