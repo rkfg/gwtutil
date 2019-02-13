fce5503f079bc95efe71b5cdce8c506de8e8cc44
Sergey Shpikin <serge@shpikin>
2019-02-13 10:30:27 +0300

    Add keypress handling to TypeaheadWrapper

--- src/main/java/ru/ppsrk/gwt/bootstrap/client/TypeaheadWrapper.java
+++ src/main/java/ru/ppsrk/gwt/bootstrap/client/TypeaheadWrapper.java
@@ -1,6 +1,6 @@
 package ru.ppsrk.gwt.bootstrap.client;
 
-import static ru.ppsrk.gwt.shared.SharedUtils.*;
+import static ru.ppsrk.gwt.shared.SharedUtils.getObjectFromCollectionById;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -10,6 +10,7 @@ import com.github.gwtbootstrap.client.ui.TextBox;
 import com.github.gwtbootstrap.client.ui.Typeahead;
 import com.github.gwtbootstrap.client.ui.Typeahead.UpdaterCallback;
 import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
@@ -20,11 +21,41 @@ import ru.ppsrk.gwt.client.HasListboxValue;
 
 public class TypeaheadWrapper<T extends HasListboxValue> extends DecoratorBase<Typeahead> {
 
+    public interface FillHandler {
+        public void fill(String text);
+    }
+
+    private FillHandler fillHandler;
+    private int minSuggestLength;
+
+    public int getMinSuggestLength() {
+        return minSuggestLength;
+    }
+
+    public void setMinSuggestLength(int minSuggestLength) {
+        this.minSuggestLength = minSuggestLength;
+    }
+
+    public void setFillHandler(FillHandler fillHandler) {
+        this.fillHandler = fillHandler;
+    }
+
     public TypeaheadWrapper() {
     }
 
     public TypeaheadWrapper(Typeahead typeahead) {
         decorated = typeahead;
+    }
+
+    public void init() {
+        getTextBox().addKeyPressHandler(e -> {
+            if (fillHandler != null && e.getNativeEvent().getKeyCode() != KeyCodes.KEY_ENTER) {
+                String text = getText();
+                if (text.length() >= minSuggestLength) {
+                    fillHandler.fill(text);
+                }
+            }
+        });
     }
 
     /**
